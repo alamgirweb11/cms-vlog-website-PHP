3341ae08aece9beb1cf506f9da0c41edda92bf6b
alamgirweb11 <alamgirweb11@gmail.com>
2020-07-27 15:11:28 +0600

    initial upload

--- admin/index.php
+++ admin/index.php
@@ -1,39 +1,177 @@
 <?php include "includes/admin_header.php"; ?>
-
-    <div id="wrapper">
-        <!-- Navigation -->
-        <?php include 'includes/admin_menubar.php'; ?>
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                         Welcome to Dashboard
-                         <small><?php echo $_SESSION['username']; ?></small>
-                        </h1>
-                      <!--   <ol class="breadcrumb">
-                          <li>
-                              <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                          </li>
-                          <li class="active">
-                              <i class="fa fa-file"></i> Blank Page
-                          </li>
-                      </ol> -->
-                    </div>
-                </div>
-                <!-- /.row -->
-
-            </div>
-            <!-- /.container-fluid -->
-
+<div id="wrapper">
+  <!-- Navigation -->
+  <?php include 'includes/admin_menubar.php'; ?>
+  <div id="page-wrapper">
+    <div class="container-fluid">
+      <!-- Page Heading -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header">
+          Welcome to Dashboard
+          <small><?php echo $_SESSION['username']; ?></small>
+          </h1>
         </div>
-        <!-- /#page-wrapper -->
-
+      </div>
+      <!-- /.row -->
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-file-text fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <?php
+                  $query = "SELECT * FROM posts";
+                  $select_all_post = mysqli_query($connection,$query);
+                  $post_counts = mysqli_num_rows($select_all_post);
+                  echo "<div class='huge'>$post_counts</div>";
+                  ?>
+                  <div>Posts</div>
+                </div>
+              </div>
+            </div>
+            <a href="posts.php">
+              <div class="panel-footer">
+                <span class="pull-left">View Details</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="panel panel-green">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-comments fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <?php
+                  $query = "SELECT * FROM comments";
+                  $select_all_comments = mysqli_query($connection,$query);
+                  $comment_counts = mysqli_num_rows($select_all_comments);
+                  echo "<div class='huge'>$comment_counts</div>";
+                  ?>
+                  <div>Comments</div>
+                </div>
+              </div>
+            </div>
+            <a href="comments.php">
+              <div class="panel-footer">
+                <span class="pull-left">View Details</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="panel panel-yellow">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-user fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <?php
+                  $query = "SELECT * FROM users";
+                  $select_all_users = mysqli_query($connection,$query);
+                  $user_counts = mysqli_num_rows($select_all_users);
+                  echo "<div class='huge'>$user_counts</div>";
+                  ?>
+                  <div> Users</div>
+                </div>
+              </div>
+            </div>
+            <a href="users.php">
+              <div class="panel-footer">
+                <span class="pull-left">View Details</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="panel panel-red">
+            <div class="panel-heading">
+              <div class="row">
+                <div class="col-xs-3">
+                  <i class="fa fa-list fa-5x"></i>
+                </div>
+                <div class="col-xs-9 text-right">
+                  <?php
+                  $query = "SELECT * FROM categories";
+                  $select_all_categories = mysqli_query($connection,$query);
+                  $category_counts = mysqli_num_rows($select_all_categories);
+                  echo "<div class='huge'>$category_counts</div>";
+                  ?>
+                  <div>Categories</div>
+                </div>
+              </div>
+            </div>
+            <a href="categories.php">
+              <div class="panel-footer">
+                <span class="pull-left">View Details</span>
+                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="clearfix"></div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- /.row -->
+       <?php 
+            // show all draft value in graph chart
+         // query for draft posts
+            $query = "SELECT * FROM posts WHERE post_status = 'draft'";
+          $select_all_draft_post = mysqli_query($connection,$query);
+          $draft_post_counts = mysqli_num_rows($select_all_draft_post);
+        // query for draft comments 
+           $query = "SELECT * FROM comments WHERE comment_status = 'unapproved'";
+          $select_all_draft_comment = mysqli_query($connection,$query);
+          $draft_comment_counts = mysqli_num_rows($select_all_draft_comment);
+        // query for draft users
+           $query = "SELECT * FROM users WHERE user_role = 'Subscriber'";
+          $select_all_draft_user = mysqli_query($connection,$query);
+          $draft_user_counts = mysqli_num_rows($select_all_draft_user);
+        ?>
+      <div class="row">
+        <script type="text/javascript">
+        google.charts.load('current', {'packages':['bar']});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+        ['Data', 'Count'],
+          <?php 
+               $element_text = ['Active Posts','Draft Posts','Comments','Pending Comments','Users','Subscribers','Categories'];
+               $element_count = [$post_counts,$draft_post_counts,$comment_counts,$draft_comment_counts,$user_counts,$draft_user_counts,$category_counts];
+               for($i=0; $i<7; $i++){
+                    echo "['{$element_text[$i]}' "  . ","  . "{$element_count[$i]}],";
+               }
+           ?>
+      //  ['Posts', 1030]
+        ]);
+        var options = {
+        chart: {
+        title: '',
+        subtitle: '',
+        }
+        };
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+        }
+        </script>
+        <div id="columnchart_material" style="width: auto; height: 500px;"></div>
+      </div>
     </div>
-    <!-- /#wrapper -->
-
-   <?php include 'includes/admin_footer.php'; ?>
+    <!-- /.container-fluid -->
+  </div>
+  <!-- /#page-wrapper -->
+</div>
+<!-- /#wrapper -->
+<?php include 'includes/admin_footer.php'; ?>
